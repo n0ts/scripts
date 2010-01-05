72340d269b63f78c007e60c71344564ec8ea570a
Naoya NAKAZAWA <naoya.n@gmail.com>
2010-01-05 11:36:14 +0900

    Fixed a bit

--- disable_service.sh
+++ disable_service.sh
@@ -3,6 +3,6 @@ function get_cmd() {
   echo 'acpid auditdi autofs avahi-daemon bluetooth cups firstboot gpm hidd ip6tables mcstrans mdmonitor netfs nfslock pcscd restorecond rpcgssd rpcidmapd sendmail xfs yum-updatesd'
 }
 for cmd in `get_cmd`; do
-  chkconfig $cmd off
+  sudo /sbin/chkconfig $cmd off
   /etc/init.d/$cmd stop
 done
