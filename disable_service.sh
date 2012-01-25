6d1ab157e879a67d4e2c56f91f6e5bb14fc8c7f5
Naoya Nakazawa <naoya.n@gmail.com>
2012-01-25 19:57:14 +0900

    Update disable_service.sh

--- disable_service.sh
+++ disable_service.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
-function get_cmd() {
-  echo 'acpid apmd auditdi autofs avahi-daemon bluetooth cups firstboot gpm hidd ip6tables
+function get_services() {
+  echo 'acpid auditdi autofs avahi-daemon bluetooth cups firstboot gpm hidd ip6tables iscsi iscsid mcstrans mdmonitor netfs nfslock pcscd restorecond rpcgssd rpcidmapd sendmail xfs yum-updatesd'
+}
+
+function get_virt_services() {
+  echo 'acpid apmd autofs cpuspeed cups gpm isdn kudzu mdmonitor microcode_ctl pcmcia rawdevices readahead readahead_early smartd'
+}
+
+for cmd in `get_services`; do
+  /sbin/chkconfig $cmd off
+  /etc/init.d/$cmd stop
+done
+
+for cmd in `get_virt_services`; do
+  /sbin/chkconfig $cmd off
+  /etc/init.d/$cmd stop
+done
